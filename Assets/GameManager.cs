37719b405c3a0c1e8dd0d6b6cc4fbe3c7bfd5714
KimDongHwan <oms2385@gmail.com>
2020-12-11 00:12:51 +0900

    Initial commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public ItemBox[] itemBoxes;
+    public bool isGameEnd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGameEnd = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(isGameEnd)
+        {
+            return;
+        }
+        
+        bool isGood = true;
+        for(int i = 0; i < 3; i++){
+            if(!itemBoxes[i].isCollide)
+            {
+                isGood = false;
+            }
+        }
+
+        if(isGood == true){
+            Debug.Log("게임 끝");
+            isGameEnd = true;
+        }
+    }
+}
